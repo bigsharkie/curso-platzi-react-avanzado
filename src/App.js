3a2ea8263637e22c3d27509b795e3f0d0f3003d4
Joel Mendez <joel.mendez@gmail.com>
2020-01-29 16:07:14 +1100

    completed lesson 31

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
-import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
-import { Home } from './pages/Home'
-import { Detail } from './pages/Detail'
-import { Router } from '@reach/router'
 import { NavBar } from './components/NavBar'
 
-export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
+import { Home } from './pages/Home'
+import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
 
+import { Router } from '@reach/router'
+import Context from './Context'
+
+export const App = () => {
   return (
     <div>
       <GlobalStyle />
       <Logo />
       <Router>
         <Home path="/" />
-        <Home path="/pet/:id" />
+        <Home path="/pet/:categoryId" />
         <Detail path="/detail/:detailId" />
       </Router>
+
+      <Context.Consumer>
+        {({ isAuth }) =>
+          isAuth ? (
+            <Router>
+              <Favs path="/favs" />
+              <User path="/user" />
+            </Router>
+          ) : (
+            <Router>
+              <NotRegisteredUser path="/favs" />
+              <NotRegisteredUser path="/user" />
+            </Router>
+          )
+        }
+      </Context.Consumer>
+
       <NavBar />
     </div>
   )
